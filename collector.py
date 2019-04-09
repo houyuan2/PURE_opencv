311c2f4a7069fe2bd973deedac809cd44f9dba5c
seedship <rnai2@illinois.edu>
2019-04-08 20:40:54 -0500

    debugging serial port

--- collector.py
+++ collector.py
@@ -3,18 +3,47 @@ from serial import Serial
 import time
 import csv
 
-ser = serial.Serial('../../../dev/ttyACM0')
-ser.flushInput()
+
+ser = serial.Serial('/dev/ttyACM0',115200,timeout=1)
+# ser.flushInput()
 
 while True:
     try:
         ser_bytes = ser.readline()
-        # decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
-        print(ser_bytes)
-        # print(decoded_bytes)
-        # with open("test_data.csv","a") as f:
-        #     writer = csv.writer(f,delimiter=",")
-        #     writer.writerow([time.time(),decoded_bytes])
+        print(ser_bytes[0:len(ser_bytes)-2].decode('utf-16'))
     except:
         print("Keyboard Interrupt")
+        ser.close()
         break
+ser.close()
+    # ser_bytes = ser.readline()
+    # print(ser_bytes.decode('utf-16'))
+
+
+# ser = serial.Serial('../../../dev/ttyACM0', 115200) # Establish the connection on a specific port
+# counter = 32 # Below 32 everything in ASCII is gibberish
+# while True:
+#      counter +=1
+#      ser.write(str(chr(counter))) # Convert the decimal number to ASCII then send it to the Arduino
+#      print(ser.readline()) # Read the newest output from the Arduino
+#      sleep(.1) # Delay for one tenth of a second
+#      if counter == 255:
+#         counter = 32
+
+
+
+# import serial
+
+# serial_port = '/dev/ttyACM0';
+# baud_rate = 115200; #In arduino, Serial.begin(baud_rate)
+# write_to_file_path = "output.txt";
+
+# output_file = open(write_to_file_path, "w+");
+# ser = serial.Serial(serial_port, baud_rate)
+# print(ser.name)
+# while True:
+#     print(ser.read())
+#     line = ser.readline();
+#     line = line.decode("utf-8") #ser.readline returns a binary, convert to string
+#     print(line);
+# output_file.write(line);
